5272cb5fe1c0c6f6c96afaa0692d706c3d2fb78b
Chavall315 <naufalbagaskara31@gmail.com>
2025-08-07 13:15:45 +0700

    hapus regis

--- resources/views/sertifikat/index.blade.php
+++ resources/views/sertifikat/index.blade.php
@@ -25,12 +25,8 @@
                     <span class="absolute left-0 -bottom-1 h-0.5 w-0 bg-orange-300 transition-all group-hover:w-full"></span>
                 </a>
                 <a href="{{ route('login') }}"
-                   class="px-4 py-1.5 rounded bg-white/20 border border-white text-white hover:bg-white hover:text-blue-700 transition font-semibold text-sm">
-                   Login
-                </a>
-                <a href="{{ route('register') }}"
-                   class="px-4 py-1.5 rounded bg-orange-400 text-white hover:bg-orange-500 transition font-semibold text-sm">
-                   Register
+                   class="px-4 py-1.5 rounded bg-orange-500 border border-white text-white hover:bg-white hover:text-blue-700 transition font-semibold text-sm">
+                   Login Admin
                 </a>
             </nav>
 
@@ -49,13 +45,8 @@
     <div id="mobile-menu" class="md:hidden px-4 pb-4 hidden bg-blue-700">
         <a href="#" class="block py-2 text-white hover:text-orange-300">Home</a>
         <a href="#" class="block py-2 text-white hover:text-orange-300">Sertifikat</a>
-        <a href="{{ route('login') }}"
-           class="block w-full text-center py-2 mt-2 bg-white/20 border border-white text-white hover:bg-white hover:text-blue-600 transition rounded">
-            Login
-        </a>
-        <a href="{{ route('register') }}"
-           class="block w-full text-center py-2 mt-2 bg-orange-400 text-white hover:bg-orange-500 transition rounded">
-            Register
+        <a href="{{ route('login') }}" class="px-4 py-1.5 rounded bg-orange-500 border border-white text-white hover:bg-white hover:text-blue-700 transition font-semibold text-sm">
+            Login Admin
         </a>
     </div>
 </header>
