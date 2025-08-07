f90d9e6c36ade91b57687bea56e0d0ae0fe293a1
Chavall315 <naufalbagaskara31@gmail.com>
2025-08-07 13:13:53 +0700

    ytta

--- resources/views/sertifikat/index.blade.php
+++ resources/views/sertifikat/index.blade.php
@@ -84,7 +84,7 @@
                 </span>
                 <input
                     type="text"
-                    placeholder="Masukkan jenis sertifikat..."
+                    placeholder="cari berdasarkan nama atau nis"
                     class="pl-10 pr-4 py-2 w-full border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500"
                 />
             </div>
